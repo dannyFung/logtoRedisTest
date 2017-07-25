f7a201d04d6f27e959dbc2c60d93d073ffee7d3f
dannyFung <danleifeng@pku.edu.cn>
2017-07-25 16:39:25 +0800

    process by line and put them to redis

--- src/main/java/JedisTest.java
+++ src/main/java/JedisTest.java
@@ -5,6 +5,9 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import redis.clients.jedis.Jedis;
 import java.io.*;
+import java.util.*;
+
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 
 public class JedisTest {
 
@@ -12,7 +15,11 @@ public class JedisTest {
     private static JedisTest instance=new JedisTest();
     private static Jedis jedis= null;
     private static final String ipAddr = "192.168.99.116";
+//    private static final String ipAddr = "localhost";
     private static final int port = 22222;
+    private static Map<String, Integer> hashMap = new HashMap<String, Integer>();
+//    private static Map<String, Integer> hashMap;
+    private JSONArray json=null;
     private JedisTest()
     {
 
@@ -83,6 +90,10 @@ public class JedisTest {
     }
 
 
+    /**
+     * 所有的文件内容存为string 字符串，再进行解析每一行（！！不推荐此方法）
+     * @param logstring 文件内容
+     */
     public void AllLogtoRedis(String logstring)
     {
         String [] arr = logstring.split("\n");
@@ -109,12 +120,19 @@ public class JedisTest {
     public String getApplisttoString(String logstring)
     {
         String [] arr = getLogArray(logstring);
-        return arr[8];
+        return arr[9];
     }
 
+    /**
+     * 每个设备id为key，value为json中取得的app_identifier 的list，存入redis中
+     * @param key 用户设备id标识（log日志中第六个字段）
+     * @param valuestring  json字符串（log日志中最后一个字段）
+     */
     public void OneJsontoRedis(String key,String valuestring)
     {
-        JSONArray json=JSONArray.fromObject(valuestring);
+//        System.out.println(key);
+//        System.out.println(valuestring.charAt(0));
+        json=JSONArray.fromObject(valuestring);
         if(jedis.exists(key))
         {
             jedis.del(key);
@@ -122,27 +140,93 @@ public class JedisTest {
         for(int i= 0;i<json.size();i++)
         {
             JSONObject jsonObject= (JSONObject) json.get(i);
-            jedis.rpush(key,jsonObject.get("app_identifier").toString());
+            String app_value=jsonObject.get("app_identifier").toString();
+            statisticAppCount(app_value);
+            jedis.rpush(key,app_value);
         }
-
-        System.out.println("userapp_id_key:"+ key);
-        System.out.println(jedis.lrange(key,0,-1));
+//        System.out.println("userapp_id_key:"+ key);
+//        System.out.println(jedis.lrange(key,0,-1));
     }
 
+    /**
+     * 统计每个app_identifier对应的key 出现的次数
+     * @param key   app_identifier
+     */
+    public void statisticAppCount(String key)
+    {
+        if(hashMap.containsKey(key))
+        {
+            hashMap.put(key,hashMap.get(key).intValue()+1);
+        }
+
+        else
+        {
+            hashMap.put(key,1);
+        }
+    }
+
+    public void printAppCount(){
+        System.out.println("统计：一共有"+hashMap.size()+"个app应用");
+        ArrayList<Map.Entry<String,Integer>> entries= sortMap(hashMap);
+
+        Iterator<Map.Entry<String, Integer>> iter = entries.iterator();
+
+        while (iter.hasNext()) {
+            Map.Entry entry = iter.next();
+            String key = entry.getKey().toString();
+            String val = entry.getValue().toString();
+            System.out.println("key= "+key+"---"+"count= "+val);
+        }
+
+//        //直接遍历hashMap时的方法
+//        Iterator iter = hashMap.entrySet().iterator();
+//        while (iter.hasNext()) {
+//            Map.Entry entry = (Map.Entry) iter.next();
+//            String key = entry.getKey().toString();
+//            String val = entry.getValue().toString();
+//            System.out.println("key= "+key+"---"+"count= "+val);
+//        }
+    }
+
+    //hashmap<string ,int> 按照int降序排列
+    public  ArrayList<Map.Entry<String,Integer>> sortMap(Map map) {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+        Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2) {
+                return obj2.getValue() - obj1.getValue();
+            }
+        });
+        return (ArrayList<Map.Entry<String, Integer>>) entries;
+    }
 
 
 
     public static void main(String[] args){
 
-        String filename="/Users/linkedme/Documents/applist.log";
+        String filename="/Users/linkedme/Documents/linkedme/applist.log.20170718-15";
         JedisTest jedisTest=JedisTest.getInstance();
-        jedisTest.init();
+        try{
+
+
+            jedisTest.init();
+            jedisTest.LogtoStringLine(filename);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }finally {
+            jedisTest.printAppCount();
+        }
+//        String filename="/Users/linkedme/Documents/linkedme/applist.log.20170718-15";
+
+//        String filename = args[0];
+//        JedisTest jedisTest=JedisTest.getInstance();
+//        jedisTest.init();
 
 //        String content=jedisTest.LogtoString(filename);
 //        jedisTest.AllLogtoRedis(content);
 
-        jedisTest.LogtoStringLine(filename);
-
+//        jedisTest.LogtoStringLine(filename);
+//        jedisTest.printAppCount();
     }
 
 
